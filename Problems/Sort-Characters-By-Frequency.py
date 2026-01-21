0eb7d9b5c07f6612ea87f89e44de40aa77c072ee
Yousef Genedy <115221628+yousef-genedy@users.noreply.github.com>
2026-01-22 00:23:06 +0200

    [451] [Time Beats: 6.34%] [Memory Beats: 5.00%] - LeetPush

--- Problems/Sort-Characters-By-Frequency.py
+++ Problems/Sort-Characters-By-Frequency.py
@@ -1,10 +1,15 @@
 1class Solution:
 2    def frequencySort(self, s: str) -> str:
-3        count = dict(sorted(Counter(s).items(), key=lambda item: item[1], reverse=True))
-4        
-5        ans = ""
+3        count = Counter(s)
+4        bucket = defaultdict(list)
+5
 6        for k, v in count.items():
-7            ans += k * v
+7            bucket[v].append(k)
 8
-9        return ans
-10
+9        res = []
+10        for i in range(len(s), 0, -1):
+11            for c in bucket[i]:
+12                res.append(c * i)
+13
+14        return ''.join(res)
+15
