0e5ee0ec2f605006288525b30d5b591f92925fbc
Yousef Genedy <115221628+yousef-genedy@users.noreply.github.com>
2026-02-18 15:08:22 +0200

    [693] [Time Beats: 100.00%] [Memory Beats: 25.94%] - LeetPush

--- Problems/Binary-Number-with-Alternating-Bits.py
+++ Problems/Binary-Number-with-Alternating-Bits.py
@@ -1,10 +1,5 @@
 1class Solution:
 2    def hasAlternatingBits(self, n: int) -> bool:
-3        n = bin(n)[2:]
-4
-5        for a, b in pairwise(n):
-6            if a == b:
-7                return False
-8
-9        return True
-10
+3        x = n ^ (n >> 1)
+4        return x & (x + 1) == 0
+5
