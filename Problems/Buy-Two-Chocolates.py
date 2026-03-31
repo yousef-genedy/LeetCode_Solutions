d3853921bcd468497a82608a5d34af9c11e25e3b
Yousef Genedy <115221628+yousef-genedy@users.noreply.github.com>
2026-03-31 22:39:21 +0200

    [2706] [Time Beats: 100.00%] [Memory Beats: 40.41%] - LeetPush

--- Problems/Buy-Two-Chocolates.py
+++ Problems/Buy-Two-Chocolates.py
@@ -1,17 +1,15 @@
 1class Solution:
 2    def buyChoco(self, prices: List[int], money: int) -> int:
-3        prices.sort()
-4
-5        min1 = math.inf
-6        min2 = math.inf
-7
-8        for p in prices:
-9            if p <= min1:
-10                min2 = min1
-11                min1 = p
-12            elif p < min2:
-13                min2 = p
-14
-15        min_cost = min1 + min2
-16        return money - min_cost if money >= min_cost else money
-17
+3        min1 = math.inf
+4        min2 = math.inf
+5
+6        for p in prices:
+7            if p <= min1:
+8                min2 = min1
+9                min1 = p
+10            elif p < min2:
+11                min2 = p
+12
+13        min_cost = min1 + min2
+14        return money - min_cost if money >= min_cost else money
+15
